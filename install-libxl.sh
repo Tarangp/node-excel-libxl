a3d24fa803d406442b3b03f08fb12a3247913804
Tarang Patel <Tarangp@users.noreply.github.com>
2013-10-27 09:51:19 +0200

    OS X compatibility

--- install-libxl.sh
+++ install-libxl.sh
@@ -3,18 +3,11 @@
 if [ ! -d deps/libxl ] ; then
   mkdir deps
   cd deps
-  wget http://www.libxl.com/download/libxl.tar.gz
+  wget http://www.libxl.com/download/libxl-mac.tar.gz
   tar zxf libxl.tar.gz
   rm -rf libxl.tar.gz
   mv libxl* libxl
   cd ..
-
-  uname -i | grep "64" 1>/dev/null
-  if [ "$?" -eq "0" ] ; then
-    ARCHPATH="lib64"
-  else
-    ARCHPATH="lib"
-  fi
-
-  sudo cp deps/libxl/$ARCHPATH/libxl.so /usr/lib
+  
+  sudo cp deps/libxl/lib/libxl.dylib /usr/lib
 fi
